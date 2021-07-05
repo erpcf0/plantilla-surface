69455db6b55b74dc1a20d186ce0881b119f0a7a7
erland <er_pcfe@outlook.es>
2021-07-05 14:50:59 -0400

    se adiciono credo

--- mix.exs
+++ mix.exs
@@ -47,6 +47,7 @@ defmodule MyApp.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:surface, "0.5.0"}
     ]
   end
